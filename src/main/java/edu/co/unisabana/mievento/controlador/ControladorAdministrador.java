c14298af9e0bed11efabc7ed4c62ac36e2e7f876
Santiago Santafe <andressasi@unisabana.edu.co>
2024-05-18 19:22:07 -0500

    Update ControladorAdministrador to use IPersonalRepository for personal-related operations

--- src/main/java/edu/co/unisabana/mievento/controlador/ControladorAdministrador.java
+++ src/main/java/edu/co/unisabana/mievento/controlador/ControladorAdministrador.java
@@ -3,14 +3,18 @@ package edu.co.unisabana.mievento.controlador;
 import edu.co.unisabana.mievento.entities.personal.Personal;
 import edu.co.unisabana.mievento.entities.usuario.Cliente;
 import edu.co.unisabana.mievento.repository.IClientRepository;
+import edu.co.unisabana.mievento.repository.IPersonalRepository;
 import edu.co.unisabana.mievento.servicio.ServicioCliente;
 import edu.co.unisabana.mievento.servicio.ServicioPersonal;
+
+import org.aspectj.internal.lang.annotation.ajcDeclareAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.ArrayList; // Add this import statement
 
 @RestController
@@ -19,7 +23,7 @@ public class ControladorAdministrador {
     @Autowired
     private IClientRepository servicioCliente;
     @Autowired
-    private ServicioPersonal servicioPersonal;
+    private IPersonalRepository servicioPersonal;
     
     @GetMapping(path = "/getUsers")
     public ResponseEntity<List<Cliente>> obtenerClientes() {
@@ -30,31 +34,40 @@ public class ControladorAdministrador {
     }
 
 
-    @GetMapping(path = "/personal/todos")
+    @GetMapping(path = "/getPersonal")
     public ResponseEntity<List<Personal>> obtenerPersonal() {
-        List<Personal> personalList = servicioPersonal.obtenerPersonal();
+        List<Personal> personalList = new ArrayList<>();
+        Iterable<Personal> iterablePersonal = servicioPersonal.findAll();
+        iterablePersonal.forEach(personalList::add);
         return new ResponseEntity<>(personalList, HttpStatus.OK);
     }
+
     @GetMapping(path = "/personal/{id}")
-    public ResponseEntity<Personal> obtenerPersonalPorId(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(servicioPersonal.obtenerPersonalPorId(id),HttpStatus.OK);
+    public ResponseEntity<Personal> obtenerPersonalPorId(@PathVariable("id") int id) {
+        Optional<Personal> optionalPersonal = servicioPersonal.findById(id);
+        if (optionalPersonal.isPresent()) {
+            return new ResponseEntity<>(optionalPersonal.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
 
     @PostMapping(path = "/personal/guardar")
     public ResponseEntity<String> guardarPersonal(@RequestBody Personal personal) {
-        servicioPersonal.guardarOEditarPersonal(personal);
+        //servicioPersonal.guardarOEditarPersonal(personal);
         return new ResponseEntity<>("Personal guardado con Exito", HttpStatus.CREATED);
     }
     @PutMapping(path = "/personal/modificar")
     public ResponseEntity<String> modificarPersonal(@RequestBody Personal personal) {
-        servicioPersonal.guardarOEditarPersonal(personal);
+        //servicioPersonal.guardarOEditarPersonal(personal);
         return new ResponseEntity<>("Personal modificado con éxito", HttpStatus.OK);
     }
 
 
     @DeleteMapping("/personal/eliminar/{id}")
     public ResponseEntity<Void> eliminarPersonal(@PathVariable("id") Long id) {
-        servicioPersonal.eliminarPersonal(id);
+        //servicioPersonal.eliminarPersonal(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
